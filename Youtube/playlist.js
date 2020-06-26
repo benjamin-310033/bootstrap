cf37c6d351618db876d3aeab93d22d058165cbe5
benjamin-310033 <67451771+benjamin-310033@users.noreply.github.com>
2020-06-26 20:12:20 +0800

    Update playlist.js

--- Youtube/playlist.js
+++ Youtube/playlist.js
@@ -1,14 +1,11 @@
 //YouTube影片ID
 let playList = [
-    "uDx3jWmdXtc", //swim
-    "IAe-SFJ4jV0", //全放空
-    "3IpN4V-JZm0" //飄向北方
+    "uDx3jWmdXtc" //swim
+
 
 ];
 //播放起訖秒數
 let playTime = [
-    [8,16],
-    [129,139],
-    [85,93]  
+    [8,16]
 
 ]
